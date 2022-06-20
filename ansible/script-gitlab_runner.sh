77424e91fe8850fa37d10429df767324e23cc959
Marek Mikolajczyk <lalala@localhost.loca>
2022-06-20 13:19:46 +0200

    update

--- ansible/script-gitlab_runner.sh
+++ ansible/script-gitlab_runner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i ../inventory/vm/hosts roles-gitlab_runner.yml -e @secrets.yml --start-at-task="register shared runner"
+ansible-playbook -i ../inventory/vm/hosts roles-gitlab_runner.yml -e @secrets.yml  -vvv
