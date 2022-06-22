b9e11aae84ce33501dc82f6369d3b1728ffb6727
Marek Mikolajczyk <lalala@localhost.loca>
2022-06-22 12:54:26 +0200

    update

--- pipelines/scripts/deploy_www_content.sh
+++ pipelines/scripts/deploy_www_content.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-'pipeline result <b>v002</b><p>' | tee  /var/www/html/index.html
-"<i>$MY_MESSAGE</i><p>" | tee -a /var/www/html/index.html
-"<i>environment $MY_ENVIRONMENT</i>" | tee -a /var/www/html/index.html
-"for project $CI_PROJECT_NAME by $CI_RUNNER_DESCRIPTION at $CI_PIPELINE_CREATED_AT<p>" | tee -a /var/www/html/index.html
-"hosted on $GITLAB_PLATFORM" | tee -a /var/www/html/index.html
+echo 'pipeline result <b>v002</b><p>' | tee  /var/www/html/index.html
+echo "<i>$MY_MESSAGE</i><p>" | tee -a /var/www/html/index.html
+echo "<i>environment $MY_ENVIRONMENT</i>" | tee -a /var/www/html/index.html
+echo "for project $CI_PROJECT_NAME by $CI_RUNNER_DESCRIPTION at $CI_PIPELINE_CREATED_AT<p>" | tee -a /var/www/html/index.html
+echo "hosted on $GITLAB_PLATFORM" | tee -a /var/www/html/index.html
