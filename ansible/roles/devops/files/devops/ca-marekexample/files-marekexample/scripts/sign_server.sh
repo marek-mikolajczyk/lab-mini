0ce346244e13f3cff61dd44ddfb008ac0af46c00
Marek Mikolajczyk <lalala@localhost.loca>
2022-07-01 23:40:54 +0200

    update

--- ansible/roles/devops/files/devops/ca-marekexample/files-marekexample/scripts/sign_server.sh
+++ ansible/roles/devops/files/devops/ca-marekexample/files-marekexample/scripts/sign_server.sh
@@ -3,9 +3,10 @@
 openssl x509 -req \
   -days 365 \
   -in ../server/"$1".marekexample.com/"$1".marekexample.com.csr \
-  -CA ../CA/ca-marekexample.crt \
-  -CAkey ../CA/ca-marekexample.key \
+  -CA /root/CA/ca-marekexample.crt \
+  -CAkey /root/CA/ca-marekexample.key \
   -CAcreateserial \
+	-CAserial /root/CA/ca-marekexample.srl \
   -extensions req_ext \
   -extfile ../server/"$1".marekexample.com/"$1".marekexample.com.cnf \
   -out ../server/"$1".marekexample.com/"$1".marekexample.com.crt
